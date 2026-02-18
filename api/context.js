7d176968dd2b828cb3b9298da604aaba12fdce6b
shprc <schepp.rick@gmail.com>
2026-02-18 08:13:04 -0600

    Create context.js

---
+++ api/context.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  // Just return empty context for now
+  return res.json({ context: {}, corrections: [], people: [], projects: [] });
+}
